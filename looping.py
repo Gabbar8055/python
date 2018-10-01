58f615c14254ca592e2a70a7e8481e51847ebbae
Mayank Singh <39026182+Gabbar8055@users.noreply.github.com>
2018-10-01 23:13:49 +0530

    edited else condition

--- looping.py
+++ looping.py
@@ -4,7 +4,9 @@ for i in list:
             print("yep, its two")
         elif  i == 1:
             print("yes its 1")
+        elif i == 3:
+            print("yes its 3")
         else:
-           print(i)
+           print("Not a part of List")
 
        
