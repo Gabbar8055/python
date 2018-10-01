e188ba5d1f422e4ba749efc5bef624d557c6dfce
Mayank Singh <39026182+Gabbar8055@users.noreply.github.com>
2018-10-01 23:22:13 +0530

    added elif condition

--- conditionals.py
+++ conditionals.py
@@ -1,6 +1,9 @@
-list_a=[1,2,3]
+list_a = [1,2,3]
 for i in list_a:
     if (i**2)%3 == 0:
      print('your number is 9')
+    elif (i**2)%3 != 0:
+     print("Your number is not 9")
      print(i)
+        
 
